d6d31f741a9b08c5eea10253a027e91eb869bbd6
NagurBabu143 <shaiknagurbabu00@gmail.com>
2025-02-24 18:26:42 +0530

    updating total file

--- jasoncard/src/productcard/productcard.jsx
+++ jasoncard/src/productcard/productcard.jsx
@@ -1,30 +1,38 @@
-
 import React, { useState } from "react";
+import "./Productcard.css";
 
-const Images = [{id: 1, Image:"./keyboardcover.jpg", name: "keyboardcover", },
-    {id: 2, Image:"./keyboardcover.jpg", name: "cover" },
-    {id: 3, Image:"./keyboardcover.jpg", name: "system keyboard" }
+const images = [
+  { id: 1, Image: "./keyboardcover.jpg", name: "keyboardcover" },
+  { id: 2, Image: "./keyboardcover.jpg", name: "cover" },
+  { id: 3, Image: "./keyboardcover.jpg", name: "system keyboard" },
+];
 
-]
+function Productcard() {
+  const [search, setSearch] = useState("");
 
+  const filteredImages = images.filter((image) =>
+    image.name.toLowerCase().includes(search.toLowerCase())
+  );
 
-
-function Productcard(){
-    const [search,setsearch]=useState("");
-
-    const filterdimages=Images.filter((Images)=>
-        Images.name.toLowerCase().includes(search.toLocaleLowerCase()));
-    
-    return(
-       <div>
-        <input type="text" placeholder="seach images" value={search} onChange={(e)=>setsearch(e.target.value)}/>
-        {filterdimages.map((Image)=>(
-            <div>
-                <img src={Image.id}/>
-            </div>
+  return (
+    <div className="productcard-container">
+      <input
+        type="text"
+        placeholder="Search images"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+        className="search-input"
+      />
+      <div className="images-container">
+        {filteredImages.map((image) => (
+          <div key={image.id} className="image-card">
+            <img src={image.Image} alt={image.name} className="product-image" />
+            <p>{image.name}</p>
+          </div>
         ))}
-       </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export default Productcard
+export default Productcard;
