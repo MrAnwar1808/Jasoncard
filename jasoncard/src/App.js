2bd59653cb7bd4d46c8dbeab419d0ccee40e1bf4
MrAnwar1808 <shaikanwar1808@gmail.com>
2025-02-24 17:31:54 +0530

    New file

---
+++ jasoncard/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Productcard from './productcard/productcard';
+
+function App() {
+  return (
+    <div >
+      <Productcard/>
+    </div>
+  );
+}
+
+export default App;
---
+++ jasoncard/src/productcard/productcard.jsx
@@ -0,0 +1,40 @@
+
+import React from "react";
+
+// temcards = [{id: 1, Image:"./keyboardcover.jpg", name: "keyboardcover" },
+//     {id: 2, Image:"./keyboardcover.jpg", name: "cover" },
+//     {id: 3, Image:"./keyboardcover.jpg", name: "system keyboard" }
+
+// ]
+
+
+function Productcard(){
+
+
+   
+
+    return(
+//         <div className="card-container">   
+//           <ul>
+//         {itemcards.map(itemcard, key)  
+
+//             <li key={itemcard.id}>
+//                 <img src={itemcard.image} alt="keyboard" />
+//                 <p>{itemcard.name}</p>
+//             </li>
+
+
+//         }}
+
+// </ul>
+
+
+//         </div>
+
+<div>
+
+</div>
+    )
+}
+
+export default Productcard
